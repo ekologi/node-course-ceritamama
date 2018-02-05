899cce22dfca4d5e3a5fa10bd3621af751ed6a04
ebaskom <ebaskom@gmail.com>
2018-02-05 19:58:13 +0700

    handle auth

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
+const passport = require('passport');
+
+
+//passport config
+require('./config/passport')(passport);
+
+//load routes
+const auth = require('./routes/auth');
+
 const app = express();
+
+//use routes
+app.use('/auth',auth);
+
 
 app.get('/', (req, res) => {
     res.send('Halo');
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:'mongodb://ebaskara:kepu00@ds123728.mlab.com:23728/ceritamama-dev',
+    googleClientID: '1038818009879-ogrd7vkfit3cljaq2e5m7juplv438lub.apps.googleusercontent.com',
+    googleClientSecret: 'fxdf8X9czFWSDiCSh5qsp_Ae'
+}
